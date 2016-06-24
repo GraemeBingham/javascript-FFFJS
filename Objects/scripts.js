23bb4b758a691cada267b4a49c7fb7a883c8855c
GraemeBingham <binghamgraeme@gmail.com>
2016-06-24 12:02:29 +1000

    Finished Object exercises

--- Objects/scripts.js
+++ Objects/scripts.js
@@ -1,3 +1,22 @@
 //Use this file to implement Part One of your project
+var animal = {};
 
+animal.username = "cat";
 
+animal["tagline"] = "Best cat ever!";
+
+noises = [];
+
+animal["noises"] = noises ;
+
+console.log(animal);
+
+for (var key in animal) {
+  if (animal.hasOwnProperty(key)) {
+    if (key === "username") {
+      console.log("Hi my name is " + animal[key]);
+    } if (key === "tagline") {
+      console.log("I like to say " + animal[key]);
+    }
+  }
+}
